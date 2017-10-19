83ca9a6a5eee1dab151bf85927e18999c2a6b923
sim756 <sim756@live.com>
2017-10-19 12:34:08 +0600

    PiranhaSearch

---
+++ sim756.Piranha/PiranhaSearch/Cache.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PiranhaSearch
+{
+    public class Cache : Dictionary<string, object>
+    {
+        
+    }
+}
---
+++ sim756.Piranha/PiranhaSearch/Search.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PiranhaSearch
+{
+    public class Search
+    {
+        public string Path { get; set; }
+
+        public string Pattern { get; set; } = "*.*";
+
+
+    }
+}
